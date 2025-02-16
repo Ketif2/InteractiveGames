d71c703e8df2d6f3a6a6fd69a453e338c56517e2
Ketif2 <kevin.revelo01@epn.edu.ec>
2025-02-15 20:18:03 -0500

    uninstall toaster dependences

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes/AppRoutes';
-import { Toaster } from 'react-hot-toast';
 import './App.css';
 
 function App() {
