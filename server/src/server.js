6ffbfc1cffc8f8ce7d1eacdd1d870364f6d59c83
Ketif2 <kevin.revelo01@epn.edu.ec>
2025-03-30 23:42:43 -0500

    Improve CORS configuration by setting the origin dynamically based on environment variable

--- server/src/server.js
+++ server/src/server.js
@@ -22,11 +22,8 @@ const __dirname = path.resolve();
 
 // Configuración de CORS mejorada
 app.use(cors({
-  // origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  origin: true,
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(cookieParser());
